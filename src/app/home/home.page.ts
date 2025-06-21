b4c7993637bcb73dfa28a258e3dee1bcda5b2988
Orcusss777 <if23.fickryramadhan@mhs.ubpkarawang.ac.id>
2025-06-21 16:29:20 +0700

    update homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
   userName: string = 'Pasien';
+  sapaan: string = '';
 
   ngOnInit() {
     const userData = JSON.parse(localStorage.getItem('userData') || '{}');
     this.userName = userData?.name || 'Pasien';
+
+    const hour = new Date().getHours();
+    if (hour < 12) this.sapaan = 'Selamat Pagi';
+    else if (hour < 18) this.sapaan = 'Selamat Siang';
+    else this.sapaan = 'Selamat Malam';
   }
 }
