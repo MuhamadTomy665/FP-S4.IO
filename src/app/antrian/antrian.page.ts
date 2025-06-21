28160b1fd506e62f84d78f3ca4b8b139241c3c04
JidRahman20 <if23.ajidhamid@mhs.ubpkarawang.ac.id>
2025-06-21 15:38:47 +0700

    update

--- src/app/antrian/antrian.page.ts
+++ src/app/antrian/antrian.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Pusher from 'pusher-js';
 
 @Component({
   selector: 'app-antrian',
@@ -15,6 +16,8 @@ export class AntrianPage implements OnInit {
   kode = '';
   barcode = '';
 
+  toastOpen = false; // 🔔 untuk menampilkan notifikasi
+
   ngOnInit() {
     const user = JSON.parse(localStorage.getItem('user') || '{}');
     const antrianData = JSON.parse(localStorage.getItem('antrian') || '{}');
@@ -26,9 +29,20 @@ export class AntrianPage implements OnInit {
       this.poli = antrianData.poli;
       this.tanggal = this.formatTanggal(antrianData.tanggal);
       this.jam = antrianData.jam;
-      this.nomorAntrian = '#' + antrianData.nomor_antrian;
+      this.nomorAntrian = antrianData.nomor_antrian;
       this.barcode = barcodeData;
       this.kode = kodeAntrian;
+
+      const pusher = new Pusher('e848914deeea58639b29', {
+        cluster: 'mt1',
+      });
+
+      const channel = pusher.subscribe('antrian-channel');
+      channel.bind('PasienDipanggil', (event: any) => {
+        if (event.antrian.nomor_antrian === this.nomorAntrian) {
+          this.toastOpen = true; // 🔔 tampilkan notifikasi
+        }
+      });
     }
   }
 
